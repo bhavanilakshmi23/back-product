d1bf6bd01ccc4272e930fb63b319f6a399b6bbee
bhavani <127599832+bhavanilakshmi23@users.noreply.github.com>
2024-01-11 18:09:46 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,128 @@
+const express = require('express');
+const { MongoClient } = require('mongodb');
+
+
+const app = express();
+const port = 3000;
+app.use(express.json());
+// MongoDB connection URL
+const mongoUrl = 'mongodb+srv://root:root23@cluster0.rnxg0b2.mongodb.net/';
+const dbName = 'eshop';
+const collectionName = 'products';
+
+app.get('/products', async (req, res) => {
+  try {
+    // Connect to MongoDB
+    const client = await MongoClient.connect(mongoUrl, { useNewUrlParser: true });
+    const db = client.db(dbName);
+
+    // Get data from the collection
+    const collection = db.collection(collectionName);
+    const data = await collection.find().toArray();
+
+    // Close the connection
+    client.close();
+
+    // Send the data as JSON response
+    res.json(data);
+  } catch (error) {
+    console.error('Error:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+
+app.get('/products/:id', async (req, res) => {
+  try {
+    // Connect to MongoDB
+    const client = await MongoClient.connect(mongoUrl, { useNewUrlParser: true });
+    const db = client.db(dbName);
+
+    // Get data from the collection
+      const collection = db.collection(collectionName);
+      const { id } = req.params;
+      const data = await collection.findOne({ id: id });
+
+    // Close the connection
+    client.close();
+
+    // Send the data as JSON response
+    res.json(data);
+  } catch (error) {
+    console.error('Error:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+
+app.delete('/products/:id', async (req, res) => {
+  try {
+    // Connect to MongoDB
+    const client = await MongoClient.connect(mongoUrl, { useNewUrlParser: true });
+    const db = client.db(dbName);
+
+    // Get data from the collection
+      const collection = db.collection(collectionName);
+      const { id } = req.params;
+      const data = await collection.deleteOne({ id: id });
+
+    // Close the connection
+    client.close();
+
+    // Send the data as JSON response
+    res.json(data);
+  } catch (error) {
+    console.error('Error:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+app.post('/products', async (req, res) => {
+  try {
+    // Connect to MongoDB
+    const client = await MongoClient.connect(mongoUrl, { useNewUrlParser: true });
+    const db = client.db(dbName);
+
+    // Get data from the collection
+      const collection = db.collection(collectionName);
+      const newproduct= req.body;
+      const data = await collection.insertMany(newproduct);
+
+    // Close the connection
+    client.close();
+
+    // Send the data as JSON response
+    res.json(data);
+  } catch (error) {
+    console.error('Error:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+app.put('/products/:id', async (req, res) => {
+  try {
+    // Connect to MongoDB
+    const client = await MongoClient.connect(mongoUrl, { useNewUrlParser: true });
+    const db = client.db(dbName);
+
+    // Get data from the collection
+      const collection = db.collection(collectionName);
+      const { id } = req.params;
+      const updateproduct = req.body;
+      const data = await collection.updateOne({id:id},{$set: updateproduct});
+
+    // Close the connection
+    client.close();
+
+    // Send the data as JSON response
+    res.json(data);
+  } catch (error) {
+    console.error('Error:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
